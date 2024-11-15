1d2f06e8521732f9beaf1d96d484f613a78a0f6f
Anabel Marinho <anabel.soares2001@gmail.com>
2024-11-15 17:02:55 -0300

    cardmine3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Cardmine from "@/components/ui/cardmine";
 import Cardmine2 from "@/components/ui/cardmine2";
+import Cardmine3 from "@/components/ui/cardmine3";
 import ContainerButtons from "@/components/ui/containerbuttons";
 
 export default function Home() {
@@ -7,7 +8,10 @@ export default function Home() {
     <div className="flex flex-col justify-center items-center mt-10 p-5 space-y-10">
       <ContainerButtons />
       <Cardmine />
+      <div className="w-full flex flex-wrap justify-center gap-5">
       <Cardmine2 />
+      <Cardmine3 />
+      </div>
     </div>
   );
 }
---
+++ src/components/ui/cardmine3/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Cardmine3 = () => {
+  return (
+    <div className="w-full max-w-[350px] h-auto bg-branco flex flex-col sm:flex-row items-center p-2 rounded-sm shadow-md
+    shadow-cinza border-b-4 border-vermelho cursor-pointer hover:bg-[#ec273f]/10 
+    transform transition-transform duration-300 hover:scale-105">
+
+      <div className="text-center space-y-1 p-2 sm:max-w-[200px]">
+        <h1 className="text-xl text-vermelho font-semibold">Anabel Marinho</h1>
+        <p className="text-xs text-vermelho">Desenvolvedora Web</p>
+        <p className="text-xs text-cinza">Sou desenvolvedora web, apaixonada por criar sites e aplicações funcionais e responsivas.</p>
+      </div>
+
+      <div className="mt-4 sm:mt-0 sm:ml-4">
+        <img src="/img/gato1.jpg" className="w-[100px] h-[100px] rounded-lg" />
+      </div>
+    </div>
+  );
+};
+
+export default Cardmine3;
+
+
