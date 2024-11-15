f53e88548163deec968a9175c774a16c363a3acf
Anabel Marinho <anabel.soares2001@gmail.com>
2024-11-15 16:33:46 -0300

    minecard2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Cardmine from "@/components/ui/cardmine";
+import Cardmine2 from "@/components/ui/cardmine2";
 import ContainerButtons from "@/components/ui/containerbuttons";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center mt-10 p-5 space-y-10 h-screen">
+    <div className="flex flex-col justify-center items-center mt-10 p-5 space-y-10">
       <ContainerButtons />
       <Cardmine />
+      <Cardmine2 />
     </div>
   );
 }
